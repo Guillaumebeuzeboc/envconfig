96481f309d048095a36d9005e819e3e125cfe5f9
Guillaume beuzeboc <guillaume.beuzeboc@gmail.com>
2019-03-27 19:59:20 +0100

    fix(ROS): adapt it to melodic

--- ros_install.zsh
+++ ros_install.zsh
@@ -2,13 +2,13 @@
 
 rosi() {
 
-    sudo apt install "ros-kinetic-$1"
+    sudo apt install "ros-melodic-$1"
 
 }
 
 _rosi() {
     local cur=${COMP_WORDS[COMP_CWORD]}
-    local repositories=$(sed -n 's/Package: ros-kinetic-\([a-z_0-9-]*\)$/\1/p' /var/lib/apt/lists/packages.ros.org.*|sort|uniq)
+    local repositories=$(sed -n 's/Package: ros-melodic-\([a-z_0-9-]*\)$/\1/p' /var/lib/apt/lists/packages.ros.org_*|sort|uniq)
     COMPREPLY=($(compgen -W "${repositories}" -- ${cur}))
     return 0
 }
