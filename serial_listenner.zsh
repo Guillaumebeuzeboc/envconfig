82ff61362ac16d0f50e7aefbcccb3f9edff3a6b4
Guillaume beuzeboc <guillaume.beuzeboc@gmail.com>
2019-06-03 12:04:18 +0200

    feat(serial_monitor): use complete device name

--- serial_listenner.zsh
+++ serial_listenner.zsh
@@ -1,8 +1,8 @@
 serial_listenner(){
     #socat stdio $(/dev/$1) | grep -v $("$2")
     if [ "$#" -eq 2 ]; then
-    	socat stdio /dev/$1 | grep -v "$2"
+    	socat stdio $1 | grep -v "$2"
     else
-    	socat stdio /dev/$1 
+    	socat stdio $1 
     fi
 }
