a1c66824aff773dd1227f7006c216917420938b5
Guillaume beuzeboc <guillaume.beuzeboc@stilla.fr>
2021-12-19 18:45:04 +0100

    improve serial_monitor

--- serial_monitor.zsh
+++ serial_monitor.zsh
@@ -1,8 +1,8 @@
 serial_monitor(){
     #socat stdio $(/dev/$1) | grep -v $("$2")
     if [ "$#" -eq 2 ]; then
-    	socat stdio $1 | grep -v "$2"
+    	socat stdio $1,b115200,raw | grep -v "$2"
     else
-    	socat stdio $1 
+    	socat stdio $1,b115200,raw 
     fi
 }
