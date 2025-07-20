2ccbdff706611a78467d078b0c67be467b5a5462
Guillaume beuzeboc <guillaume.beuzeboc@gmail.com>
2025-07-20 19:31:25 +0200

    no need to be sudo

--- serial_monitor.zsh
+++ serial_monitor.zsh
@@ -1,8 +1,8 @@
 serial_monitor(){
     #socat stdio $(/dev/$1) | grep -v $("$2")
     if [ "$#" -eq 2 ]; then
-      sudo socat stdio $1,b115200,raw | grep -v "$2"
+      socat stdio $1,b115200,raw | grep -v "$2"
     else
-      sudo socat stdio $1,b115200,raw
+      socat stdio $1,b115200,raw
     fi
 }
