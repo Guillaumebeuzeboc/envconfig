100439f491d31717de22b47288487ae24af26b5c
Guillaume beuzeboc <guillaume.beuzeboc@gmail.com>
2025-08-07 12:32:36 +0200

    remote charmcraft

--- remote_snapcraft.zsh
+++ remote_snapcraft.zsh
@@ -21,6 +21,27 @@ remote_snapcraft_clean() {
     ssh -t titan@titan.local "cd ~/snapcraft_runs/${start_dir}; rm -Rf *" 
 }
 
+remote_charmcraft() {
+    local start_path
+    local start_dir
+    start_path=$(pwd)
+    start_dir=$(basename ${start_path})
+    cd ../
+    rsync -rz --hard-links --perms --executability --delete --force --links "./${start_dir}" titan@titan.local:/home/titan/charmcraft_runs/
+    cd -
+    ssh -t titan@titan.local "cd ~/charmcraft_runs/${start_dir}; charmcraft pack --verbose --debug"
+    scp "titan@titan.local:/home/titan/charmcraft_runs/${start_dir}/*.charm" .
+}
+
+remote_charmcraft_clean() {
+    local start_path
+    local start_dir
+    start_path=$(pwd)
+    start_dir=$(basename ${start_path})
+    ssh -t titan@titan.local "cd ~/charmcraft_runs/${start_dir}; charmcraft clean --verbose"
+    ssh -t titan@titan.local "cd ~/charmcraft_runs/; rm -Rf ${start_dir}"
+}
+
 poweroff_titan() {
     ssh -t titan@titan.local "sudo poweroff" 
 }
